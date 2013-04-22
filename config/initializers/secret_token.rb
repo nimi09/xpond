dc8d712d9022f4a53417f0f812b7ce7f5856c10c
nimi09 <nicu.mihailescu@gmail.com>
2013-04-22 11:41:34 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xpond::Application.config.secret_token = '1f0d0102815e43ec8d2aac86930e55c2703ffa6c9f432400fb30af6a271495d5f1d18998b078e4c0f7389f51a0a58fec7ba65ee38aa21fe92fa25947705eada5'
