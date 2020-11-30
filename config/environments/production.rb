89cde485d2424789ba47276494f48d57a2917d3c
nimi09 <nicu.mihailescu@gmail.com>
2020-11-30 10:29:27 +0100

    renames application.css back and adds configs to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,12 @@ Xpond::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?  
+
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
