29bb681eea082f57888e7943b7a05372be95c769
nimi09 <nicu.mihailescu@gmail.com>
2020-11-30 10:56:40 +0100

    added sth in assets/config/manifest.js and run rake assets:precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Xpond::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
